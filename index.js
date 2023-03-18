7ca56cf36ca8f0c3184af1447d570261605dc2ed
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-03-18 18:03:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// file system
+const fileSystem = require('fs');
+
+fileSystem.mkdir('./teste', function() {
+    console.log('Criei o diretório teste');
+})
+
+fileSystem.writeFile('./teste/abcd.txt', 'Olá, tudo bem ? ', function() {
+    console.log('Criei um novo arquivo no diretório teste com o nome abcd.txt');
+})
+
+fileSystem.readFile('./teste/abcd.txt', function(err, data) {
+    console.log('O meu arquivo possui os dados: ' + data.toString());
+})
+
+
+
+
+const x = 1+ 1;
+console.log('O valor de 1 + 1 é: ' + x)
