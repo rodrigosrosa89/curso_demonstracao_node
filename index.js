2c50a97c1b4c0500e2bb550ee37c6e5f4ce85cac
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-03-18 18:38:09 -0300

    Criando próprio módulo

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // file system
 const fileSystem = require('fs');
 
-fileSystem.mkdir('./teste', function() {
+fileSystem.mkdir('./teste', function () {
     console.log('Criei o diretório teste');
 })
 
-fileSystem.writeFile('./teste/abcd.txt', 'Olá, tudo bem ? ', function() {
+fileSystem.writeFile('./teste/abcd.txt', 'Olá, tudo bem ? ', function () {
     console.log('Criei um novo arquivo no diretório teste com o nome abcd.txt');
 })
 
-fileSystem.readFile('./teste/abcd.txt', function(err, data) {
+fileSystem.readFile('./teste/abcd.txt', function (err, data) {
     console.log('O meu arquivo possui os dados: ' + data.toString());
 })
 
@@ -19,5 +19,16 @@ console.log('Data atual');
 console.log(moment().format());
 console.log(moment().format('dd-mm-yyyy'));
 
-const x = 1+ 1;
+const x = 1 + 1;
 console.log('O valor de 1 + 1 é: ' + x)
+
+//Importando o módulo criado:
+const calc = require('./meuModulo/funcoes.js')
+
+const numero1 = 10;
+const numero2 = 1;
+
+console.log('Subtracao entre os numeros: ' + numero1 + ' e ' + numero2);
+console.log(calc.sub(numero1, numero2));
+console.log('Adição entre os numeros: ' + numero1 + ' e ' + numero2);
+console.log(calc.add(numero1, numero2));
---
+++ meuModulo/funcoes.js
@@ -0,0 +1,13 @@
+module.exports = {
+    sub: subtracao,
+    add: adicao,
+};
+
+
+function subtracao(n1, n2) {
+    return n1 - n2;
+}
+
+function adicao(n1, n2) {
+    return n1 + n2;
+}
