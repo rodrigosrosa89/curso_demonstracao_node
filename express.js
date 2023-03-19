dac3888d4b3e7abf946801e9ea5a52c725eee764
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-03-19 10:10:14 -0300

    Incluindo Middleware

--- express.js
+++ express.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello world!'));
+
+const logMiddleware = function (req, res, next) {
+    console.log('Api recebeu alguma informação');
+
+    next();
+};
+
+app.use(express.static('./site'));
+
+app.use(logMiddleware);
+
+app.get('/', function (req, res) {
+    res.send('Hello world!')
+});
 
 app.get('/pt', (req, res) => res.send('Olá mundo!'));
 
 app.post('/post', function (req, res) {
     res.send({
-        status : 'ok'
+        status: 'ok'
     });
 });
 
-app.listen(port, () => 
-console.log('Example app listening at http://localhost:${port})')
+app.listen(port, () =>
+    console.log('Example app listening at http://localhost:${port})')
 );
