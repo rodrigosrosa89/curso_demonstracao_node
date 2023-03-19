a979f6fbe49b928ac04cc811c9a674b75a9ad5f5
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-03-19 11:08:38 -0300

    Utilizando webSocket

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const webSocket = require('ws');
 
 const logMiddleware = function (req, res, next) {
     console.log('Api recebeu alguma informação');
